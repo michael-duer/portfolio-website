a358d6c7a9b134663d0cfb4dbd547e33f7649b0b
Michael Dür <62062370+michael-duer@users.noreply.github.com>
2022-10-10 23:30:52 +0200

    add projects add project information as component project.js display projects in a list

---
+++ my-portfolio/src/projects.js
@@ -0,0 +1,101 @@
+export const projects = [
+  {
+    name: "cannabis-tincture-calculator",
+    description: "shiny website to calculate cannabis tincture",
+    languages: [
+        "R",
+        "Shiny"
+    ],
+    concepts: "shiny website",
+    commits: 2,
+    date: "october 2022",
+    repositoryIsPublic: "true",
+    linkGithub: "https://github.com/michael-duer/cannabis-tincture-calculator",
+    linkLive: "https://my-collection-of.shinyapps.io/cannabis-tincture-calculator/",
+    image: "Bild"
+  },
+  {
+    name: "pokemon-app",
+    description: "display information about each pokemon with data from the pokeapi",
+    languages: [
+        "React",
+        "HTML",
+        "Javascript",
+        "CSS"          
+    ],
+    concepts: "request and display data from API",
+    commits: 8,
+    date: "october 2022",
+    repositoryIsPublic: "true",
+    linkGithub: "https://github.com/michael-duer/pokemon-app",
+    linkLive: "none",
+    image: "Bild"
+  },
+  {
+    name: "sudoku",
+    description: "website to play sudoku",
+    languages: [
+        "HTML",
+        "CSS",
+        "Javascript"
+    ],
+    concepts: "game, different design themes",
+    commits: 7,
+    date: "juli 2021",
+    repositoryIsPublic: "true",
+    linkGithub: "https://github.com/michael-duer/sudoku",
+    linkLive: "none",
+    image: "Bild"
+  },
+  {
+    name: "open-data-project",
+    description: "data visualization of Bernmobil passenger data from 2019 and 2020 in comparison",
+    languages: [
+        "D3.js",
+        "Javascript",
+        "HTML",
+        "CSS",
+        "python"       
+    ],
+    concepts: "data visualization with D3.js",
+    commits: 33,
+    date: "juni 2021",
+    repositoryIsPublic: "true",
+    linkGithub: "https://github.com/michael-duer/open-data-project",
+    linkLive: "https://fahrgastdaten-bernmobil.opendata.iwi.unibe.ch/",
+    image: "Bild"
+  },
+  {
+    name: "website for 'naturheilpraxis Rita Dür'",
+    description: "client website for a naturopath located in Burgdorf",
+    languages: [
+        "HTML",
+        "CSS",
+        "Javascript"
+    ],
+    concepts: "website for a client",
+    commits: 100,
+    date: "ongoing",
+    repositoryIsPublic: "false",
+    linkGithub: "https://github.com/michael-duer/website-naturheilpraxis",
+    linkLive: "https://naturheilpraxis-v3.herokuapp.com/home",
+    image: "Bild"
+  },
+  {
+    name: "money-dashboard",
+    description: "data visualization of money spending",
+    languages: [
+        "D3.js",
+        "Javascript",
+        "HTML",
+        "CSS"  
+    ],
+    concepts: "data visualization with D3.js",
+    commits: 6,
+    date: "januar 2022",
+    repositoryIsPublic: "false",
+    linkGithub: "https://github.com/michael-duer/money-dashbord",
+    linkLive: "none",
+    image: "Bild"
+  },
+];
